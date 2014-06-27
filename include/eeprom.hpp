7071b961f6efca5b158b529e238ada7ad9f530df
Ivan Dubrov <dubrov.ivan@gmail.com>
2014-06-27 10:16:53 -0700

    Working on EEPROM emulation

--- include/eeprom.hpp
+++ include/eeprom.hpp
@@ -8,7 +8,10 @@ class eeprom
 public:
 	enum Status
 	{
-		Ok = 0, Error
+		Ok = 0,
+		NoPage,
+		NotFound,
+		Error
 	};
 public:
 	constexpr eeprom(uint32_t base, uint16_t page_count) :
@@ -17,22 +20,41 @@ public:
 	}
 	Status initialize();
 
+	Status read(uint16_t tag, uint16_t& data);
+	Status write(uint16_t tag, uint16_t data);
+
 private:
-	void rescue_data();
-	uint16_t find_page();
+	FLASH_Status rescue_if_full(int16_t page);
+	int16_t find_current();
 
 	uint16_t page_status(int16_t page)
 	{
-		return *(__IO uint16_t*) (_base + (page * PageSize));
+		return *(__I uint16_t*) (_base + page * PageSize);
 	}
 
-	FLASH_Status eeprom::erase_page(int16_t page)
+	FLASH_Status set_page_status(int16_t page, uint16_t status)
+	{
+		return FLASH_ProgramHalfWord(_base + page * PageSize, status);
+	}
+
+	/// Erase given page, but only if it's "dirty" (has any non-'1' bit).
+	FLASH_Status erase_page(int16_t page)
 	{
 		return dirty(page) ?
 				FLASH_ErasePage(_base + page * PageSize) : FLASH_COMPLETE;
 	}
 
 	bool dirty(int16_t page);
+
+	bool search(uint32_t start, uint32_t end, uint16_t tag)
+	{
+		for (uint32_t offset = start; offset < end; offset += 4)
+		{
+			if (*(__I uint16_t*) offset == tag)
+				return true;
+		}
+		return false;
+	}
 
 private:
 	uint32_t const _base;
@@ -41,11 +63,8 @@ private:
 	// Empty page, could be used for rescueing data from the current page.
 	// Once rescue process is started, the page is marked as 'rescue target'
 	static constexpr uint16_t ErasedPage = 0xffff;
-	// This is the page being used to rescue data from the current page.
-	// During the initialization, all such pages are erased.
-	static constexpr uint16_t TargetPage = 0xeeee;
 	// This is the current page we are writing data to.
-	static constexpr uint16_t CurrentPage = 0x0000;
+	static constexpr uint16_t CurrentPage = 0xABCD;
 
 #if defined (STM32F10X_LD) || defined (STM32F10X_LD_VL) || \
 	defined (STM32F10X_MD) || defined(STM32F10X_MD_VL)
@@ -55,14 +74,14 @@ private:
 	defined (STM32F10X_XL) || defined (STM32F10X_CL)
 	static constexpr uint32_t PageSize = 0x800; // 2KByte
 #endif
-	class Unlock
+	class FlashUnlock
 	{
 	public:
-		Unlock()
+		FlashUnlock()
 		{
 			FLASH_Unlock();
 		}
-		~Unlock()
+		~FlashUnlock()
 		{
 			FLASH_Lock();
 		}
--- src/eeprom.cpp
+++ src/eeprom.cpp
@@ -3,63 +3,146 @@
 
 eeprom::Status eeprom::initialize()
 {
-	Unlock unlock; // Unlock flash
+	FlashUnlock unlock; // Unlock flash
 
-	int16_t current = -1;
+	int16_t current = find_current();
 	for (int16_t page = 0; page < _page_count; page++)
 	{
-		uint16_t status = page_status(page);
-		if (status == CurrentPage)
-		{
-			if (current == -1)
-			{
-				// Found page marked as current
-				current = page;
-			}
-			else if (current == -2)
-			{
-				// Got at least two pages marked as current, erase all pages.
-				if (erase_page(page) != FLASH_COMPLETE)
-					return Error;
-			}
-			else
-			{
-				// We've got at least two pages, marked as current. Something is wrong.
-				// Clear all the pages and reinitialize from scratch.
-				if (erase_page(current) != FLASH_COMPLETE)
-					return Error;
-				if (erase_page(page) != FLASH_COMPLETE)
-					return Error;
-				current = -2;
-			}
-		}
-		else if (status == ErasedPage || status == TargetPage)
-		{
-			if (erase_page(page) != FLASH_COMPLETE)
-				return Error;
-		}
+		// Erase all pages except current. Note that although we might have several
+		// pages marked as current (in case power was switched off while we were
+		// rescuing data), only first one will be preserved.
+		if (page != current && erase_page(page) != FLASH_COMPLETE)
+			return Error;
 	}
 
 	if (current < 0)
 	{
-		// We don't have current page, make first page one.
-		if (FLASH_ProgramHalfWord(_base, CurrentPage) != FLASH_COMPLETE)
+		// We don't have current page, mark first page as one.
+		if (set_page_status(0, CurrentPage) != FLASH_COMPLETE)
 			return Error;
-	} else
+	}
+	else
 	{
-		// Check if current page is full
-		// FIXME:...
+		rescue_if_full(current);
 	}
 	return Ok;
+}
+
+int16_t eeprom::find_current()
+{
+	for (int16_t page = 0; page < _page_count; page++)
+	{
+		if (page_status(page) == CurrentPage)
+			return page;
+	}
+	return -1;
 }
 
 bool eeprom::dirty(int16_t page)
 {
 	for (uint32_t offset = 0; offset < PageSize; offset += 2)
 	{
-		uint16_t value = *(__IO uint16_t*) (_base + (page * PageSize) + offset);
+		uint16_t value = *(__I uint16_t*) (_base + (page * PageSize) + offset);
 		if (value != 0xffff)
 			return true;
 	}
 	return false;
+}
+
+FLASH_Status eeprom::rescue_if_full(int16_t page)
+{
+	// Check if last word of the page was written or not
+	uint32_t src_base = _base + page * PageSize;
+	uint32_t src_last = src_base + PageSize - 4;
+	FLASH_Status status;
+
+	// Page is not full yet
+	if (*(__I uint32_t*) src_last == 0xffffffff)
+		return FLASH_COMPLETE;
+
+	// Last word is not 0xffffffff, we need to rescue to the next page
+	int16_t tgt_page = page + 1;
+	if (tgt_page >= _page_count)
+		tgt_page -= _page_count;
+
+	// Destination address
+	uint32_t tgt_first = _base + tgt_page * PageSize + 4;
+	uint32_t tgt_addr = tgt_first;
+
+	// Start scanning source page from the end (to get the latest value)
+	for (uint32_t offset = src_last; offset > src_base; offset -= 4)
+	{
+		uint16_t tag = *(__I uint16_t*) offset;
+		// Entry is empty or value is in the target page already, continue
+		if (tag == 0xffff || search(tgt_first, tgt_addr, tag))
+			continue;
+
+		// Copy data
+		uint16_t data = *(__I uint16_t*) (offset + 2);
+		if ((status = FLASH_ProgramHalfWord(tgt_addr + 2, data))
+				!= FLASH_COMPLETE)
+			return status;
+		if ((status = FLASH_ProgramHalfWord(tgt_addr, tag)) != FLASH_COMPLETE)
+			return status;
+		tgt_addr += 4;
+	}
+
+	// Mark target page as current
+	if ((status = FLASH_ProgramHalfWord(src_base, CurrentPage))
+			!= FLASH_COMPLETE)
+		return status;
+
+	// Erase source page
+	if ((status = FLASH_ErasePage(src_base)) != FLASH_COMPLETE)
+		return status;
+
+	return FLASH_COMPLETE;
+}
+
+eeprom::Status eeprom::read(uint16_t tag, uint16_t& data)
+{
+	int16_t current = find_current();
+	if (current < 0)
+		return NoPage;
+
+	uint32_t base = _base + current * PageSize;
+	uint32_t last = base + PageSize - 4;
+	// Start scanning source page from the end (to get the latest value)
+	for (uint32_t offset = last; offset > base; offset -= 4)
+	{
+		uint16_t t = *(__I uint16_t*) offset;
+		if (tag == t)
+		{
+			data = *(__I uint16_t*) (offset + 2);
+			return Ok;
+		}
+	}
+	return NotFound;
+}
+
+eeprom::Status eeprom::write(uint16_t tag, uint16_t data)
+{
+	int16_t current = find_current();
+	if (current < 0)
+		return NoPage;
+
+	uint32_t base = _base + current * PageSize;
+	uint32_t last = base + PageSize - 4;
+	// Start scanning source page from the end (to get the latest value)
+	for (uint32_t offset = base + 4; offset <= last; offset += 4)
+	{
+		uint16_t t = *(__I uint16_t*) offset;
+		if (t == 0xffff)
+		{
+			if (FLASH_ProgramHalfWord(offset + 2, data) != FLASH_COMPLETE)
+				return Error;
+			if (FLASH_ProgramHalfWord(offset, tag) != FLASH_COMPLETE)
+				return Error;
+			break;
+		}
+	}
+
+	if (rescue_if_full(current) != FLASH_COMPLETE)
+		return Error;
+	return Ok;
 }
