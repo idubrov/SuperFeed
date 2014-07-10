c310fb54b3c320bf039a7420210f4a1b5db0d94d
Ivan Dubrov <dubrov.ivan@gmail.com>
2014-07-10 14:02:48 -0700

    Using tuple from the std

--- include/tui/menu/menu.hpp
+++ include/tui/menu/menu.hpp
@@ -2,6 +2,7 @@
 #define __MENU_HPP
 
 #include <utility>
+#include <tuple>
 #include <type_traits>
 
 #include "util.hpp"
@@ -76,7 +77,9 @@ class menu
 		}
 	};
 
-	typedef util::tuple<Actions...> actions_tuple;
+	typedef std::tuple<Actions&&...> actions_tuple;
+	constexpr static std::size_t actions_count =
+			std::tuple_size<actions_tuple>::value;
 public:
 	menu(tui::console& console, Actions&&... actions) :
 			console(console), actions(std::forward<Actions>(actions)...), scroll(
@@ -84,7 +87,7 @@ public:
 	{
 		static_assert(traits::assert_valid_actions<
 				typename std::remove_reference<Actions>::type...>(
-				(typename std::remove_reference<Actions>::type*)nullptr...),
+						(typename std::remove_reference<Actions>::type*)nullptr...),
 				"Not a valid action list");
 	}
 
@@ -99,14 +102,50 @@ public:
 		}
 	}
 private:
+
+	template<unsigned N, typename ...Args>
+	struct Apply
+	{
+		template<template<typename, typename ... > class F>
+		static void apply(actions_tuple& actions, unsigned idx, Args&&... args)
+		{
+			if (idx > 0)
+			{
+				Apply<N + 1, Args...>::template apply<F>(actions, idx - 1, args...);
+			}
+			else
+			{
+				using H = typename std::tuple_element<N, actions_tuple>::type;
+				F<H, Args...>::apply(std::get<N>(actions), args...);
+			}
+		}
+
+	};
+
+	template<typename ...Args>
+	struct Apply<actions_count, Args...>
+	{
+		template<template<typename, typename ... > class F>
+		static void apply(actions_tuple&, unsigned, Args&&...)
+		{
+			// FIXME: Fail at runtime!!!
+		}
+	};
+
+	template<typename ...Args>
+	Apply<0, Args...> make_apply(Args&&...)
+	{
+		return Apply<0, Args...>();
+	}
+
 	void redraw()
 	{
 		auto& lcd = console.lcd();
 		lcd.clear();
-		for (unsigned i = 0; i < 4 && i + scroll < actions_tuple::arity; i++)
+		for (unsigned i = 0; i < 4 && i + scroll < actions_count; i++)
 		{
 			lcd << hw::lcd::position(0, i) << "  ";
-			actions.template apply<Print>(i + scroll, console);
+			make_apply(console).template apply<Print>(actions, i + scroll, console);
 		}
 	}
 private:
--- include/util.hpp
+++ include/util.hpp
@@ -17,48 +17,6 @@ constexpr unsigned c_strlen(char const* str, unsigned count = 0)
 	return ('\0' == str[0]) ? count : c_strlen(str + 1, count + 1);
 }
 
-// Our own tuple implementation
-template<typename ... Items> class tuple;
-
-template<>
-class tuple<>
-{
-public:
-	typedef void item_type;
-	constexpr static unsigned arity = 0;
-public:
-	template<template<typename, typename... > class F, typename ...Args>
-	void apply(unsigned, Args&&...)
-	{
-		// FIXME: Fail!
-	}
-};
-
-template<typename H, typename ... T>
-class tuple<H, T...>
-{
-public:
-	typedef H item_type;
-	constexpr static unsigned arity = 1 + tuple<T...>::arity;
-public:
-	tuple(H&& head, T&&... tail) :
-			head(std::forward<H>(head)), tail(std::forward<T>(tail)...)
-	{
-	}
-
-	template<template<typename, typename... > class F, typename ...Args>
-	void apply(unsigned idx, Args&&... args)
-	{
-		if (idx > 0)
-			tail.apply<F, Args...>(idx - 1, args...);
-		else
-			F<H, Args...>::apply(head, args...);
-	}
-private:
-	H&& head;
-	tuple<T...> tail;
-};
-
 }
 
 #endif /* __UTIL_HPP */
